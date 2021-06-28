39e7cfbce2c8d4c350fa48bd6ab14ff6b4ad8855
Anubhav Saini <anubhavsaini@ASAIN01-PER-01.local>
2021-06-28 11:33:05 -0400

    restructured

--- GeotabMobileSDK/Sources/GeotabMobileSDK/Assets/Module.Script.js
+++
@@ -1,9 +0,0 @@
-//
-//  Module.Scripts.js
-//  GeotabDriveSDK
-//
-//  Created by Yunfeng Liu on 2020-01-02.
-//
-if (window.{{geotabModules}}.{{moduleName}} == null) {
-    window.{{geotabModules}}.{{moduleName}} = {};
-}
