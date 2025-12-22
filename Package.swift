ae70aa23eef0f3eecfaf60ee593a81ead9b306fb
Abdelrhman Eliwa <abdelrhman.eliwa@transflo.com>
2025-12-22 13:01:14 +0200

    Revert "Change iOS deployment target to be 14"
    This reverts commit 71dd8d40a538c00b1d561312523facce435d075b.

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "GeotabMobileSDK",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v14),
+        .iOS(.v15),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
