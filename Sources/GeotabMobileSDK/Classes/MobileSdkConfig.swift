00e48e7a8b32df5964c04f64cba3d4514b54f0ef
Bitrise Buildbot <builds@bitrise.io>
2026-05-07 15:02:48 +0000

    6.9.19371

--- Sources/GeotabMobileSDK/Classes/MobileSdkConfig.swift
+++ Sources/GeotabMobileSDK/Classes/MobileSdkConfig.swift
@@ -1,6 +1,6 @@
 /// :nodoc:
 public class MobileSdkConfig {
-    public static let sdkVersion = "6.9.4_19370"
+    public static let sdkVersion = "6.9.4_19371"
 }
 
 /// :nodoc:
--- Sources/GeotabMobileSDK/Classes/Modules/Auth/AuthUtil/Logging+Auth.swift
+++ Sources/GeotabMobileSDK/Classes/Modules/Auth/AuthUtil/Logging+Auth.swift
@@ -79,8 +79,7 @@ extension Logging {
             context[Auth.ContextKey.totalBytes] = systemAttributes[.systemSize] as? Int64
         }
 
-        if let keychainError = error as? KeychainError,
-           case .saveFailed(let osStatus) = keychainError {
+        if let osStatus = extractKeychainSaveStatus(from: error) {
             context[Auth.ContextKey.osStatus] = Int(osStatus)
         }
 
@@ -96,6 +95,25 @@ extension Logging {
             tags: tags,
             context: context
         )
+    }
+
+    private func extractKeychainSaveStatus(from error: (any Error)?) -> OSStatus? {
+        guard let error else { return nil }
+        if let keychainError = error as? KeychainError,
+           case .saveFailed(let osStatus) = keychainError {
+            return osStatus
+        }
+        if let authError = error as? AuthError {
+            switch authError {
+            case .tokenRefreshFailed(_, let underlying, _, _):
+                return extractKeychainSaveStatus(from: underlying)
+            case .failedToSaveAuthState(_, let underlying, _):
+                return extractKeychainSaveStatus(from: underlying)
+            default:
+                return nil
+            }
+        }
+        return nil
     }
 }
 
